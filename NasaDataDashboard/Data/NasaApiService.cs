eea55017d6b6a80ebf55603a7be31ab6f37ef492
HarryWharton <harry.wharton8@gmail.com>
2025-10-06 17:21:35 +0100

    Created an empty service that will eventually get the api data

---
+++ NasaDataDashboard/Data/NasaApiService.cs
@@ -0,0 +1,14 @@
+﻿namespace NasaDataDashboard.Data
+{
+    public class NasaApiService
+    {
+        private readonly HttpClient _httpClient;
+        private const string ApiKey = "DEMO_KEY"; // when changing to actual api key remember to not commit plain text key
+
+        public NasaApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+    }
+}
