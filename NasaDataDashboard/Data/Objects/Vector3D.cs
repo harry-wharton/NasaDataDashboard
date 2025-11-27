80c2f0f28620dbbfd8900f6fbc924f1b5674ecc4
HarryWharton <harry.wharton8@gmail.com>
2025-11-27 18:59:48 +0000

    3d vector class

---
+++ NasaDataDashboard/Data/Objects/Vector3D.cs
@@ -0,0 +1,17 @@
+﻿namespace NasaDataDashboard.Data.Objects
+{
+    public class Vector3D
+    {
+        public struct Vector3d
+        {
+            public double X;
+            public double Y;
+            public double Z;
+
+            public Vector3d(double x, double y, double z) { X = x; Y = y; Z = z; }
+
+            public override string ToString() => $"({X}, {Y}, {Z})";
+        }
+
+    }
+}
